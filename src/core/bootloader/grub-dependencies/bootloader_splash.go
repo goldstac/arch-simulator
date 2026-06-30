7e8013fe45353aa233c18c4066a2660cc9508319
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-06-30 17:28:10 +0400

    add more implemention and add a go file

---
+++ src/core/bootloader/grub-dependencies/bootloader_splash.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`
+    /\
+   /  \
+  /\   \
+ /      \
+/__/\/\__\
+
+=========================
+ Arch Linux Simulator
+=========================
+`)
+}
--- src/core/bootloader/grub/grub.py
+++ src/core/bootloader/grub/grub.py
@@ -1,7 +1,18 @@
+import subprocess
 import time
 print("GRUB")
 print("1: Arch Linux")
 choose_boot_option_grub = input("Choose Boot Option : ")
 if choose_boot_option_grub == "1":
     print("Loading Linux")
+    print("Loading Ramdisk")
+    time.sleep(0.3)
+    print("#########")
+    time.sleep(0.3)
+    print("#########")
+    time.sleep(0.3)
+    splash_run = subprocess.run("cd .. && go run grub-dependencies/bootloader_splash.go",shell=True, capture_output=True,text=True)
+    print(splash_run.stdout)
+    print(splash_run.stderr)
+
     
