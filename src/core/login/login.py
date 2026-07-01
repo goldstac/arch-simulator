2dee42145ae0befe54f83b43b0024e6e298aeed7
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-01 15:14:17 +0400

    basic login

---
+++ src/core/login/login.py
@@ -0,0 +1,7 @@
+print("Welcome To Arch Linux")
+username = "admin"
+password = "iusearchbtw"
+ask_username = input("Enter Username : ")
+if username == {username}:
+    print("Username Correct")
+    ask_passwd = input ("Enter Your Password")
